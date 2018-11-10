a2b56c88d4cf28772504afc15d54ab49c5994609
Brian Francis <13bfrancis@gmail.com>
2018-11-10 13:16:39 -0500

    beginning of revised react yahtzee project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/Toolbar';
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import styled from 'styled-components';
+
+const styles = {
+  grow: {
+    flexGrow: 1
+  }
+};
+
+const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+`;
 
 class App extends Component {
+  state = {
+    value: 0
+  };
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
   render() {
+    const { value } = this.state;
+    const { classes } = this.props;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <AppBar position="relative">
+          <ToolBar>
+            <Typography
+              variant="title"
+              className={classes.grow}
+              color="secondary"
+            >
+              Yahtzee
+            </Typography>
+            <Tabs value={value} onChange={this.handleChange}>
+              <Tab label="Game" />
+              <Tab label="Scorecard" />
+            </Tabs>
+          </ToolBar>
+        </AppBar>
+        {value === 0 && <Typography variant="title">Game</Typography>}
+        {value === 1 && <Typography variant="title">Scorecard</Typography>}
+      </Container>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
